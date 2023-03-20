69ab861d1f8bcdf35b794a72cf153db3974f0c03
Anthony Setiawan 丁富勝 <anthonysetiawan.ding@gmail.com>
2023-03-20 21:49:54 +0800

    Improve unexpected error reporting for PlecoToAnki script

--- pleco_to_anki.py
+++ pleco_to_anki.py
@@ -13,19 +13,23 @@ class PlecoToAnki:
     self.output_file.write('tags:' + self.tags + "\n")
     count = 0
     for line in self.input_file:
-      word = PlecoFlashcardEntry(line).chinese_word
-      sequence = [
-        word.traditional,
-        word.english,
-        "『" + word.dashed_simplified() + "』",
-        word.zhuyin,
-        word.pinyin,
-        word.english,
-        '',
-        'Pleco Flashcards'
-      ]
-      self.output_file.write(';'.join(sequence) + "\n")
-      del word
+      try:
+        word = PlecoFlashcardEntry(line).chinese_word
+        sequence = [
+          word.traditional,
+          word.english,
+          "『" + word.dashed_simplified() + "』",
+          word.zhuyin,
+          word.pinyin,
+          word.english,
+          '',
+          'Pleco Flashcards'
+        ]
+        self.output_file.write(';'.join(sequence) + "\n")
+        del word
+      except ValueError as e:
+        e.args += (f"on input line: {count + 1} with content: \"{line}\" in file: {self.input_file.name}\n",)
+        raise
       count += 1
     print("Parsed %s entries successfully!" % (count))
 
