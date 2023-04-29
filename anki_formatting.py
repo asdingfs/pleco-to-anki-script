93fda1735638e37cde3b393ee2fd717a2fa1aa7f
Anthony Setiawan 丁富勝 <anthonysetiawan.ding@gmail.com>
2023-04-29 23:34:49 +0800

    Cleanup unused furigana formatting method from application

--- anki_formatting.py
+++ anki_formatting.py
@@ -11,15 +11,3 @@ def split_and_filter(text, delimiter):
 def emphasize_word_in_sentence(sentence, word):
   arr = sentence.split(word)
   return emphasize(word).join(arr)
-
-# return HTML text containing interleaved phonetics called ruby-style texts
-def get_rubi_element(word, furigana = ''):
-  if furigana: # is not empty
-    return "<rb>%s</rb><rp>(</rp><rt>%s</rt><rp>)</rp>"%(
-      word,
-      furigana
-    )
-  else:
-    return "<rb>%s</rb>"%(
-      word
-    )
